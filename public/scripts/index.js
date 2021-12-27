06d0d674ae106f572c5017bcd8462466ab192eb3
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-12-27 11:38:01 -0500

    Incorporated Asynchronous Loading
    Fixed a loading issue with the 3D models.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,73 +5,52 @@ let scene;
 let obj;
 let obj2;
 
-function init(){
-    container = document.querySelector('.scene');
+container = document.querySelector('.scene');
+scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+const fov = 75;
+// const asp = container.clientWidth / container.clientHeight;
+const asp = container.clientWidth / window.innerHeight;
+const near = 1;
+const far = 50;
 
-    const fov = 75;
-    // const asp = container.clientWidth / container.clientHeight;
-    const asp = container.clientWidth / window.innerHeight;
-    const near = 1;
-    const far = 50;
+camera = new THREE.PerspectiveCamera(fov, asp, near, far);
+camera.position.set(1, 1, 20);
 
-    camera = new THREE.PerspectiveCamera(fov, asp, near, far);
-    camera.position.set(1, 1, 20);
+renderer = new THREE.WebGLRenderer({antialias:true, alpha:false});
+renderer.setSize(document.body.clientWidth, document.body.clientHeight);
+// renderer.setSize(container.clientWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+container.appendChild(renderer.domElement);
+
+const loader = new THREE.GLTFLoader();
+
+async function init(){
 
     // const ambient = new THREE.AmbientLight(0x404040, 3);
     const ambient = new THREE.AmbientLight(0xffffff)
     scene.add(ambient);
 
-    var directionalLight = new THREE.DirectionalLight( 0xffffff );
-    directionalLight.position.set( 0, 1, 1 ).normalize();
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(0,1,1).normalize();
     scene.add( directionalLight );
 
-    renderer = new THREE.WebGLRenderer({antialias:true, alpha:false});
-    renderer.setSize(document.body.clientWidth, document.body.clientHeight);
-    // renderer.setSize(container.clientWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    container.appendChild(renderer.domElement);
-
-    let loader = new THREE.GLTFLoader();
-
     //Credit to Sebastian Sosnowski from Sketchfab
-    loader.load("./public/modelo/scene.gltf", (gltf) =>{
-        scene.add(gltf.scene);
-        obj = gltf.scene.children[0];
-        obj.position.set(0,0,0);
-        // obj.scale.set(0.0001, 0.00001, 0.0001);
-        renderer.render(scene, camera);
-        animate();
-        // console.log(gltf.scene.getWorldPosition(obj.position));
-        animar();
-    });
+    obj = await loader.loadAsync("./public/modelo/scene.gltf");
     //Credit to Scrunchy32205 from Sketchfab
-    loader.load("./public/earth/scene.gltf", (gltf) =>{
-        scene.add(gltf.scene);
-        obj2 = gltf.scene.children[0];
-        obj2.position.set(15,0,8);
-        renderer.render(scene, camera);
-        // animate();
-    });
+    obj2 = await loader.loadAsync("./public/earth/scene.gltf");
+    scene.add(obj.scene);
+    scene.add(obj2.scene);
+    animate(obj.scene.children[0]);
+    obj2.scene.position.set(15,0,8);
+    animate(obj2.scene.children[0]);
+    // renderer.render(scene, camera);
 
-    function animate(){
-        requestAnimationFrame(animate);
-        obj.rotation.z += 0.005;
+    function animate(objeto){
+        requestAnimationFrame(() => {animate(objeto);});
+        objeto.rotation.z += 0.005;
         renderer.render(scene, camera);
-    }
-
-    function animar() {
-        renderer.render(scene, camera);
-        // if(obj2.position.x <= 15) {
-        //     obj2.position.x += -0.02;
-        // }
-        // else {
-        //     obj2.position.x += 0.02;
-        // }
-        obj2.position.x += -0.02;
-        requestAnimationFrame(animar);
     }
 }
 
