9a22dcce7f847633e283036877b2045f37d9237d
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-12-27 18:36:38 -0500

    Implemented Polar Orbit

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,21 +12,19 @@ const fov = 75;
 // const asp = container.clientWidth / container.clientHeight;
 const asp = container.clientWidth / window.innerHeight;
 const near = 1;
-const far = 50;
-
+const far = 100;
 camera = new THREE.PerspectiveCamera(fov, asp, near, far);
-camera.position.set(1, 1, 40);
+camera.position.set(0, 0, 40);
 
 renderer = new THREE.WebGLRenderer({antialias:true, alpha:false});
 renderer.setSize(document.body.clientWidth, document.body.clientHeight);
 // renderer.setSize(container.clientWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 container.appendChild(renderer.domElement);
 
 const loader = new THREE.GLTFLoader();
 
-async function init(){
+async function main(){
 
     // const ambient = new THREE.AmbientLight(0x404040, 3);
     const ambient = new THREE.AmbientLight(0xffffff)
@@ -42,14 +40,19 @@ async function init(){
     //Credit to Scrunchy32205 from Sketchfab
     obj2 = await loader.loadAsync("./public/earth/scene.gltf");
     obj2 = obj2.scene
+
     scene.add(obj);
     scene.add(obj2);
-    spin(obj.children[0]);
-    obj2.children[0].scale.multiplyScalar(3);
-    obj2.children[0].position.set(20,0,0);
-    spin(obj2.children[0]);
-    console.log(obj2.children[0].position);
-    orbit(obj2.children[0]);
+    obj = obj.children[0];
+    obj.angle = 0;
+    obj2 = obj2.children[0];
+    obj2.angle = 0;
+
+    spin(obj);
+    obj2.scale.multiplyScalar(3);
+    obj2.position.set(20,0,0);
+    spin(obj2);
+    orbit(obj2);
     renderer.render(scene, camera);
 
     function spin(objeto){
@@ -60,21 +63,13 @@ async function init(){
 
     function orbit(objeto) {
         requestAnimationFrame(() => {orbit(objeto);});
-        if(objeto.position.z >= 0) {
-            objeto.position.x += 1;
-        }
-        else {
-            objeto.position.x -= 1;
-        }
-        if(objeto.position.x >= 0) {
-            objeto.position.z -= 1;
-        }
-        else {
-            objeto.position.z += 1;
-        }
+        objeto.angle += 0.005;
+        const off = [Math.cos(objeto.angle) * 20, Math.sin(objeto.angle) * -20];
+        objeto.position.x = off[0];
+        objeto.position.z = off[1];
         renderer.render(scene, camera);
     }
 
 }
 
-init();
+main();
