7fde44c70d3d791eb111447733519622844df061
Jake Sanghavi <57878447+jakesanghavi@users.noreply.github.com>
2021-12-27 17:00:43 -0500

    Added Orbital Animation
    To do list: make orbit circular (currently a square), find new Earth model that is not transparent

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -15,7 +15,7 @@ const near = 1;
 const far = 50;
 
 camera = new THREE.PerspectiveCamera(fov, asp, near, far);
-camera.position.set(1, 1, 20);
+camera.position.set(1, 1, 40);
 
 renderer = new THREE.WebGLRenderer({antialias:true, alpha:false});
 renderer.setSize(document.body.clientWidth, document.body.clientHeight);
@@ -34,24 +34,47 @@ async function init(){
 
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(0,1,1).normalize();
-    scene.add( directionalLight );
+    scene.add(directionalLight);
 
     //Credit to Sebastian Sosnowski from Sketchfab
     obj = await loader.loadAsync("./public/modelo/scene.gltf");
+    obj = obj.scene
     //Credit to Scrunchy32205 from Sketchfab
     obj2 = await loader.loadAsync("./public/earth/scene.gltf");
-    scene.add(obj.scene);
-    scene.add(obj2.scene);
-    animate(obj.scene.children[0]);
-    obj2.scene.position.set(15,0,8);
-    animate(obj2.scene.children[0]);
-    // renderer.render(scene, camera);
+    obj2 = obj2.scene
+    scene.add(obj);
+    scene.add(obj2);
+    spin(obj.children[0]);
+    obj2.children[0].scale.multiplyScalar(3);
+    obj2.children[0].position.set(20,0,0);
+    spin(obj2.children[0]);
+    console.log(obj2.children[0].position);
+    orbit(obj2.children[0]);
+    renderer.render(scene, camera);
 
-    function animate(objeto){
-        requestAnimationFrame(() => {animate(objeto);});
+    function spin(objeto){
+        requestAnimationFrame(() => {spin(objeto);});
         objeto.rotation.z += 0.005;
         renderer.render(scene, camera);
     }
+
+    function orbit(objeto) {
+        requestAnimationFrame(() => {orbit(objeto);});
+        if(objeto.position.z >= 0) {
+            objeto.position.x += 1;
+        }
+        else {
+            objeto.position.x -= 1;
+        }
+        if(objeto.position.x >= 0) {
+            objeto.position.z -= 1;
+        }
+        else {
+            objeto.position.z += 1;
+        }
+        renderer.render(scene, camera);
+    }
+
 }
 
 init();
